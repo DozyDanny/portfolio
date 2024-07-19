51f4bc6216bb3c9552c0b095b7e4b8b8afd45a85
DozyDanny <91636702+DozyDanny@users.noreply.github.com>
2024-07-19 18:52:51 -0500

    added rainbow text, aligned content to top of screen

--- portfolio/src/components/structural/PortfolioLayout.jsx
+++ portfolio/src/components/structural/PortfolioLayout.jsx
@@ -1,22 +1,38 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, Tooltip } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
+import '../../index.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function PortfolioLayout(props) {
 
     return (
-        <Container className="d-flex flex-column">
-            <Navbar bg="white" variant="white">
-                <Nav className="me-auto">
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="about-me">About Me</Nav.Link>
-                    <Nav.Link as={Link} to="projects">Projects</Nav.Link>
-                    <Nav.Link as={Link} to="experience">Experience</Nav.Link>
-                </Nav>
-            </Navbar>
-            <div className="mt-3">
-                <Outlet />
-            </div>
+        <Container>
+            <Row className="justify-content-md-center" style={{ margin: "3rem" }}>
+                <Navbar bg="dark" variant="dark" sticky="top" expand="sm" collapseOnSelect>
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/"><headerButton>Home</headerButton></Nav.Link>
+                        <Nav.Link as={Link} to="about-me"><headerButton>About Me</headerButton></Nav.Link>
+                        <Nav.Link as={Link} to="projects"><headerButton>Projects</headerButton></Nav.Link>
+                        <Nav.Link as={Link} to="experience"><headerButton>Experience</headerButton></Nav.Link>
+                    </Nav>
+                </Navbar>
+            </Row>
+            <Row className="justify-content-md-center">
+                <Col xs lg="2">
+                    <h1>test</h1>
+                </Col>
+                <Col md="auto">
+                    {/* Content goes here */}
+                    <Outlet />
+                </Col>
+                <Col xs lg="2">
+                    <h1>test</h1>
+                </Col>
+            </Row>
+            
         </Container>
         
     );
